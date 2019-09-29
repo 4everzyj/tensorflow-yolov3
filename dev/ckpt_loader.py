fdd17956a260e5d77311a400f4863478bd9db60e
Eddie_Zhang <eddiezyj@qq.com>
2019-09-29 16:49:48 +0800

    add ckpt_loader.py to load the ckpt and save the log for tensorboard.

---
+++ dev/ckpt_loader.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+import tensorflow as tf
+import time
+
+
+def save_graph():
+    saver = tf.train.import_meta_graph("../checkpoint/yolov3_coco.ckpt.meta", clear_devices=True)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        saver.restore(sess, "../checkpoint/yolov3_coco.ckpt")
+
+        # save
+        localtime = time.strftime("%Y%m%d_%H%M%S", time.localtime())
+        train_writer = tf.summary.FileWriter("../log/%s" % localtime, sess.graph)
+
+
+if __name__ == "__main__":
+    save_graph()
