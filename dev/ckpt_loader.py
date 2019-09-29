4f95fe0e022014c3989c913e75c4c3ac98d201e6
Eddie_Zhang <eddiezyj@qq.com>
2019-09-29 17:08:39 +0800

    load converted ckpt

--- dev/ckpt_loader.py
+++ dev/ckpt_loader.py
@@ -5,10 +5,12 @@ import time
 
 
 def save_graph():
-    saver = tf.train.import_meta_graph("../checkpoint/yolov3_coco.ckpt.meta", clear_devices=True)
+    # saver = tf.train.import_meta_graph("../checkpoint/yolov3_coco.ckpt.meta", clear_devices=True)
+    saver = tf.train.import_meta_graph("../checkpoint/yolov3_coco_demo.ckpt.meta", clear_devices=True)
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        saver.restore(sess, "../checkpoint/yolov3_coco.ckpt")
+        # saver.restore(sess, "../checkpoint/yolov3_coco.ckpt")
+        saver.restore(sess, "../checkpoint/yolov3_coco_demo.ckpt")
 
         # save
         localtime = time.strftime("%Y%m%d_%H%M%S", time.localtime())
